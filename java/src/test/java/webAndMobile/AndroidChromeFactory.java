bdb69d9c4f0d312f7fc52447f14d75567e795d8a
ssu <moorthi.subramani@sigos.com>
2017-04-03 18:16:40 +0200

    added all driver test

---
+++ java/src/test/java/webAndMobile/AndroidChromeFactory.java
@@ -0,0 +1,21 @@
+package webAndMobile;
+
+import org.testng.annotations.Factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ssu on 3/28/2017.
+ */
+public class AndroidChromeFactory {
+    @Factory
+    public Object[] createInstances() {
+
+        String appiumURL = "http://10.120.22.160:80/da/ensemble/device/88994f27-4bdd-4084-adb5-69ddccc871e3/9340/web/appium/wd/hub/";
+
+        List<Object> objList = new ArrayList<Object>();
+        objList.add(new AllDriverTest("Android", appiumURL));
+        return objList.toArray();
+    }
+}
